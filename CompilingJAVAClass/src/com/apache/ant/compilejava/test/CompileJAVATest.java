d6b521cdcf2cae3ffe7642eb3be3ae5b5a05dc96
AbhaleAmol <abhaleamol@hotmail.com>
2017-12-24 14:49:38 +0530

    Amol added Compiling JAVA Class ANT Project

---
+++ CompilingJAVAClass/src/com/apache/ant/compilejava/test/CompileJAVATest.java
@@ -0,0 +1,9 @@
+package com.apache.ant.compilejava.test;
+
+public class CompileJAVATest {
+
+	public static void main(String[] args) {
+		
+		//System.out.println("Compiled " + CompileJAVATest.class.getSimpleName() + " class successfully");
+	}
+}
